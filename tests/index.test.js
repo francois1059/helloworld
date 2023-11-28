748e208e5b48a2677efac3d3dc25d17d56f1327e
François Martin <francois.martin@fr.clara.net>
2023-11-28 13:59:58 +0100

    chore: add tests

---
+++ tests/index.test.js
@@ -0,0 +1,8 @@
+const assert = require('node:assert');
+const { describe, it } = require('node:test');
+
+describe('suite', () => {
+  it('should work', () => {
+      assert.strictEqual(1, 1);
+  });
+});
